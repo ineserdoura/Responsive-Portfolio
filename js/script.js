443f95575384f4055a206736f0119a68a759aa1e
Inês Serdoura <ineserdoura@hotmail.com>
2021-04-04 12:08:03 +0100

    Added github api to fetch projects

--- js/script.js
+++ js/script.js
@@ -50,87 +50,102 @@ function lightMode() {
 
 // get git hub api response
 
-// const myProjectsSection = document.querySelector("#my-projects");
-// const myProjectsItems = document.querySelector("#my-projects-items");
+const myProjectsSection = document.querySelector("#projects");
+const myProjectsItems = document.querySelector("#projects-items");
 
-// fetch("https://api.github.com/users/ineserdoura/repos")
-//   .then((res) => {
-//     return res.json();
-//   })
-//   .then((data) => {
-//     for (const repo of data) {
-//         console.log(data)
-//       // foreach repositorie from GitHub
-//       const repositoryUrl = repo.html_url; // repositorie url
-//       const repositoryName = repo.name.replaceAll("-", " ").toUpperCase(); // removes the - that exist in the repositorie names
-//       const projectWebsite = repo.homepage; // project website
-//       const repositoryLanguages = repo.languages_url;
-//       // create col
-//       const col = document.createElement("div");
-//       col.classList.add("col-md-4", "col-12", "mt-5");
+fetch("https://api.github.com/users/ineserdoura/repos")
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    for (const repo of data) {
+        console.log(data)
+       console.log( data.indexOf(repo))
+      
+      // foreach repositorie from GitHub
+      const repositoryUrl = repo.html_url; // repositorie url
+      const repositoryName = repo.name.replaceAll("-", " ").toUpperCase(); // removes the - that exist in the repositorie names
+      const projectWebsite = repo.homepage; // project website
+      const repositoryLanguages = repo.languages_url;
+      const descripion =repo.description;
+      // create col
+      const col = document.createElement("div");
+      col.classList.add("col-md-4", "col-12", "mt-5");
 
-//       //create card
-//       const card = document.createElement("div");
-//       card.classList.add("card", "text-center", "shadow-lg", "rounded");
+      //create card
+      const card = document.createElement("div");
+      card.classList.add("card", "text-center", "rounded");
 
-//       //create card body
-//       const cardBody = document.createElement("div");
-//       cardBody.classList.add("card-body");
+      //create card body
+      const cardBody = document.createElement("div");
+      cardBody.classList.add("card-body");
 
-//       //create card title
-//       const cardTitle = document.createElement("h6");
-//       cardTitle.textContent = `${repositoryName}`;
+      //create card title
+      const cardTitle = document.createElement("h6");
+      cardTitle.textContent = `${repositoryName}`;
 
-//       //create card subtitle / repo website
-//       const cardSubtitle = document.createElement("a");
-//       cardSubtitle.classList.add("mb-2", "text-muted");
-//       cardSubtitle.setAttribute("href", projectWebsite);
-//       cardSubtitle.setAttribute("target", "_blank");
-//       cardSubtitle.textContent = "Check the project live.";
-//       if (!projectWebsite) {
-//         cardSubtitle.style.visibility = "hidden"; // if the project doesn't have a website, hide this section
-//       }
+      //create card subtitle / repo website
+      const cardSubtitle = document.createElement("a");
+      cardSubtitle.classList.add("mb-2");
+      cardSubtitle.setAttribute("href", projectWebsite);
+      cardSubtitle.setAttribute("target", "_blank");
+      cardSubtitle.textContent = "Check the project live.";
+      if (!projectWebsite) {
+        cardSubtitle.style.visibility = "hidden"; // if the project doesn't have a website, hide this section
+      }
 
-//       // create card text / languages icons
-//       const cardText = document.createElement("h3");
-//       cardText.classList.add("card-text");
+      //create card descripion
+      const cardDescription = document.createElement("p");
+      cardDescription.classList.add("card-text");
+      cardDescription.innerHTML=`${descripion}`;
 
-//       //create card link / repository link
-//       const h3 = document.createElement("h3");
-//       const cardLink = document.createElement("a");
-//       cardLink.classList.add("card-link");
-//       cardLink.setAttribute("href", repositoryUrl);
-//       cardLink.setAttribute("target", "_blank");
+      // create card text / languages icons
+      const cardLanguages = document.createElement("h3");
+      cardLanguages.classList.add("card-text");
 
-//       //create github icon
-//       const gitHubIcon = document.createElement("i");
-//       gitHubIcon.classList.add("fab", "fa-github");
-//       cardLink.appendChild(gitHubIcon); // append github icon to link
-//       h3.appendChild(cardLink); // append link to h3
+      //create card link / repository link
+      const h3 = document.createElement("h3");
+      const cardLink = document.createElement("a");
+      cardLink.classList.add("card-link");
+      cardLink.setAttribute("href", repositoryUrl);
+      cardLink.setAttribute("target", "_blank");
 
-//       // create card
-//       cardBody.appendChild(cardTitle); // append title to card body
-//       cardBody.appendChild(cardSubtitle); // append subtitle to card body
-//       cardBody.appendChild(cardText); // append text to card body
-//       cardBody.appendChild(h3); // append h3 to card body
-//       card.appendChild(cardBody); // append card body to card
-//       col.appendChild(card); // append card to col 
-//       myProjectsItems.appendChild(col); // append col to projects section
+      //create github icon
+      const gitHubIcon = document.createElement("i");
+      gitHubIcon.classList.add("fab", "fa-github");
+      cardLink.appendChild(gitHubIcon); // append github icon to link
+      h3.appendChild(cardLink); // append link to h3
 
-//       fetch(repositoryLanguages) // get repository languages url response
-//         .then((res) => {
-//           return res.json();
-//         })
-//         .then((data) => {
-//           const repoLanguages = Object.keys(data).sort(); // create an array with the keys from the repository languages object 
-//           const h6 = document.createElement("h6"); 
-//           h6.classList.add("px-1");
-//           h6.innerHTML = repoLanguages.join(" "); // create a string from the previous array
-//           cardText.appendChild(h6); // append h6 to card text
-//         });
-//     }
-//   })
-//   .catch(() => {
-//     // if no response// error from the api hide my projects link in menu and my projects section
-//     myProjectsSection.classList.add("none");
-//   });
+      // create card
+      cardBody.appendChild(cardTitle); // append title to card body
+      cardBody.appendChild(cardSubtitle); // append subtitle to card body
+      cardBody.appendChild(cardLanguages); // append text to card body
+      cardBody.appendChild(cardDescription); // apend description
+       cardBody.appendChild(h3); // append h3 to card body
+      card.appendChild(cardBody); // append card body to card
+      col.appendChild(card); // append card to col 
+      myProjectsItems.appendChild(col); // append col to projects section
+      const repoPosition= data.indexOf(repo);
+      if(repoPosition%2==0 || repoPosition==0){
+        card.classList.add("card-even");
+      } else{
+        card.classList.add("card-odd");
+      }
+
+      fetch(repositoryLanguages) // get repository languages url response
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          const repoLanguages = Object.keys(data).sort(); // create an array with the keys from the repository languages object 
+          const h6 = document.createElement("h6"); 
+          h6.classList.add("px-1");
+          h6.innerHTML = repoLanguages.join(" "); // create a string from the previous array
+          cardLanguages.appendChild(h6); // append h6 to card text
+        });
+    }
+  })
+  .catch(() => {
+    // if no response/ error from the api hide my projects link on menu and my projects section
+    myProjectsSection.classList.add("none");
+  });
